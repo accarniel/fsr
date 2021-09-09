7eedea96fe967e57df9e79bcb019bb66747781a7
Felippe Galdino <ocfgaldino@gmail.com>
2021-09-09 19:37:25 -0300

    Improving the classes details and DESCRIPTION

--- R/fsr_base.R
+++ R/fsr_base.R
@@ -3,6 +3,15 @@
 #' @slot obj An `sfg` object.
 #' @slot md The membership degree of the component.
 #' 
+#' @details 
+#' 
+#' A `component` object is composed of two attributes. The first one is a crisp spatial
+#' object and the second one a membership degree in \]0, 1\] of this  `component`.
+#' 
+#' @references
+#'
+#' [Carniel, A. C.; Schneider, M. Spatial Plateau Algebra: An Executable Type System for Fuzzy Spatial Data Types. In Proceedings of the 2018 IEEE International Conference on Fuzzy Systems (FUZZ-IEEE 2018), pp. 1-8, 2018.](https://ieeexplore.ieee.org/document/8491565)
+#' 
 #' @import methods sf
 #' @export
 setClass("component",
@@ -17,6 +26,16 @@ setClass("component",
 #' @slot component A list of components.
 #' @slot supp An `sfg` object that stores the union of the spatial objects of the components of the spatial plateau object.
 #' @slot type The data type of the spatial plateau object.
+#' 
+#' @details 
+#' 
+#' A `pgeom` object is composed of a list of `component` objects, an `sfg` object that represents 
+#' the union of all crisp spatial objects of its components (i.e., the support), and its data
+#' type, which can be either `PLATEAUPOINT`, `PLATEAULINE`, or `PLATEAUREGION`.
+#' 
+#' @references
+#'
+#' [Carniel, A. C.; Schneider, M. Spatial Plateau Algebra: An Executable Type System for Fuzzy Spatial Data Types. In Proceedings of the 2018 IEEE International Conference on Fuzzy Systems (FUZZ-IEEE 2018), pp. 1-8, 2018.](https://ieeexplore.ieee.org/document/8491565)
 #' 
 #' @import methods
 #' @export
